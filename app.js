10764488b58358fcb025e0bf1124ff8ce3d8c16c
profgrammer <javarep4199@gmail.com>
2019-02-16 01:52:22 +0530

    'first'

---
+++ app.js
@@ -0,0 +1,53 @@
+const key = '0d383115fa814c959ada88c9360eb5f8';
+const main = document.querySelector('main');
+const sources = document.querySelector('.sourceSelector');
+const defaultSource = 'the-hindu';
+
+window.addEventListener('load', async(e) => {
+  updateNews();
+  await updateSources();
+  sources.value = defaultSource;
+
+  sources.addEventListener('change', (e) => {
+    updateNews(e.target.value);
+  })
+
+  if('serviceWorker' in navigator){
+    try{
+      navigator.serviceWorker.register('sw.js');
+      console.log('sw registered');
+    }catch(e){
+      console.log('reg failed');
+    }
+  }
+});
+
+
+async function updateSources(){
+  const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${key}
+`);
+  const json = await res.json();
+
+  sources.innerHTML = json.sources
+  .map(src => `<option value="${src.id}">${src.name}</option>`)
+  .join('\n');
+}
+
+async function updateNews(source = defaultSource){
+  const res = await fetch(`http://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${key}`);
+  const json = await res.json();
+
+  main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article){
+  return `
+  <div class="article">
+      <a href="${article.url}">
+        <h2>${article.title}</h2>
+        <img src="${article.urlToImage}" alt="${article.title}">
+        <p>${article.description}</p>
+      </a>
+    </div>
+  `;
+}
---
+++ sw.js
@@ -0,0 +1,45 @@
+const staticAssets = [
+  './',
+  './styles.css',
+  './app.js',
+  './fallback.json',
+  './images/fetch-dog.jpg'
+];
+
+self.addEventListener('install', async e => {
+    // console.log('installing');
+    const cache = await caches.open('news-static');
+    cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', e => {
+  // console.log('fetching');
+  const req = e.request;
+  const url = new URL(req.url);
+  if(url.origin === location.origin)
+  e.respondWith(cacheFirst(req));
+  else{
+    e.respondWith(networkFirst(req));
+  }
+});
+
+
+async function cacheFirst(req){
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
+}
+
+
+
+async function networkFirst(req){
+  const cache = await caches.open('news-dynamic');
+
+  try{
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch(err){
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || caches.match('fallback.json');
+  }
+}
